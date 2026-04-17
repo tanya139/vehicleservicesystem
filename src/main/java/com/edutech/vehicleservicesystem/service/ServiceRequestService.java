7f93605fc0efeb008773f9b9fd1a047c2985d9f1
yuktha-kiran <yukthakiranv@gmail.com>
2026-04-17 10:15:41 +0530

    mechfile

--- src/main/java/com/edutech/vehicleservicesystem/service/ServiceRequestService.java
+++ src/main/java/com/edutech/vehicleservicesystem/service/ServiceRequestService.java
@@ -14,7 +14,7 @@ import com.edutech.vehicleservicesystem.repository.ServiceRequestRepository;
 @Service
 public class ServiceRequestService {
 
-    @Autowired
+    @Autowiredr
     private ServiceRequestRepository serviceRequestRepository;
 
     public ServiceRequest createRequest(ServiceRequest request) {
