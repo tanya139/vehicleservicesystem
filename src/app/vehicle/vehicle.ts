c320e1c592a8215ee50c4b8835a48d73d2d269c7
yuktha-kiran <yukthakiranv@gmail.com>
2026-04-24 14:05:28 +0530

    Improved UI responsiveness and button styling across all pages

--- src/app/vehicle/vehicle.ts
+++ src/app/vehicle/vehicle.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HttpService } from '../services/http';
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-vehicle',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './vehicle.html'
 })
 export class VehicleComponent {
 
   vehicle = { model: '', number: '' };
   message = '';
-  messageType = '';
+  addedVehicle: any = null;
 
-  constructor(private http: HttpService) {}
+  constructor(private http: HttpService, private auth: AuthService) {}
 
   addVehicle() {
     this.http.post('/api/owner/vehicle', this.vehicle).subscribe({
-      next: (response) => {
+      next: (response: any) => {
         this.message = 'Vehicle added successfully!';
-        this.messageType = 'success';
+        this.addedVehicle = response;
         this.vehicle = { model: '', number: '' };
       },
       error: (err) => {
@@ -32,7 +34,7 @@ export class VehicleComponent {
         } else {
           this.message = 'Failed to add vehicle. Try again.';
         }
-        this.messageType = 'error';
+        this.addedVehicle = null;
       }
     });
   }
